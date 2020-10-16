84c8ef98b610ea4cabf46ba9a1a2f3bb922cf274
Ian Douglas Scott <idscott@system76.com>
2020-10-16 09:39:19 -0700

    improv: Use clap for argument parsing

--- src/main.rs
+++ src/main.rs
@@ -11,6 +11,7 @@ mod runtime;
 mod toolchain;
 
 use anyhow::Context;
+use clap::{App, AppSettings, Arg, ArgSettings, SubCommand};
 
 use self::{
     config::Config,
@@ -18,12 +19,10 @@ use self::{
     runtime::Runtime,
 };
 
-use std::{env::args, error::Error as _, process::exit};
+use std::{error::Error as _, process::exit};
 
 #[derive(Debug, Error)]
 pub enum Error {
-    #[error("invalid command-line usage")]
-    ArgumentUsage(#[source] anyhow::Error),
     #[error("configuration error")]
     Configure(#[source] anyhow::Error),
     #[error("an error with docker was encountered")]
@@ -31,6 +30,123 @@ pub enum Error {
 }
 
 fn main_() -> Result<(), Error> {
+    let matches = App::new("Tensorman")
+        .about("Tensorflow Docker image manager")
+        .setting(AppSettings::SubcommandRequired)
+        .setting(AppSettings::DisableVersion)
+        .setting(AppSettings::VersionlessSubcommands)
+        .arg(Arg::with_name("tag-or-container")
+            .help("+TAG_NAME or =CONTAINER_NAME")
+            .required(false)
+            .validator(|s|
+                if s.starts_with("+") || s.starts_with("=") {
+                    Ok(())
+                } else {
+                    Err(format!("'{}' doesn't start with + or =", s))
+                }
+            )
+        )
+        .subcommand(SubCommand::with_name("default")
+            .about("Defines the default tensorflow image to use when not specified")
+            .arg(Arg::with_name("tag")
+                .help("Tag")
+                .required(true)
+            )
+            .arg(Arg::with_name("variant")
+                .help("Variant(s)")
+                .possible_values(&["gpu", "python3", "jupyter"])
+                .multiple(true)
+            )
+        )
+        .subcommand(SubCommand::with_name("list")
+            .about("List tensorflow images installed on the system")
+        )
+        .subcommand(SubCommand::with_name("pull")
+            .about("Fetches and updates tensorflow images")
+            .arg(Arg::with_name("tag")
+            )
+        )
+        .subcommand(SubCommand::with_name("remove")
+            .about("Removes an image either by its sha sum, or tag")
+            .arg(Arg::with_name("image")
+                .help("Image(s) to remove")
+                .multiple(true)
+                .required(true)
+            )
+        )
+        .subcommand(SubCommand::with_name("run")
+            .about("Mounts an image and executes the given command.")
+            .after_help("Use a shell as the <cmd> for interactive sessions.")
+            .arg(Arg::with_name("cmd")
+                .help("Command to run in container")
+                .required(true)
+            )
+            .arg(Arg::with_name("arg")
+                .help("Argument(s) to command")
+                .multiple(true)
+            )
+        )
+        .subcommand(SubCommand::with_name("save")
+            .about("Saves an active container to a new image.")
+            .arg(Arg::with_name("container")
+                .help("Container to save")
+                .required(true)
+            )
+            .arg(Arg::with_name("image")
+                .help("Name of new image")
+                .required(true)
+            )
+        )
+        .subcommand(SubCommand::with_name("show")
+            .about("Show the active image that will be run")
+        )
+        .arg(Arg::with_name("force")
+            .set(ArgSettings::Global)
+            .help("Apply the subcommand by force (ie: force removal)")
+            .short("f")
+            .long("force")
+        )
+        .arg(Arg::with_name("gpu")
+            .set(ArgSettings::Global)
+            .help("Uses an image which supports GPU compute")
+            .long("gpu")
+        )
+        .arg(Arg::with_name("docker-cmd")
+            .set(ArgSettings::Global)
+            .help("Call COMMAND when invoking docker")
+            .long("docker-cmd")
+            .takes_value(true)
+        )
+        .arg(Arg::with_name("jupyter")
+            .set(ArgSettings::Global)
+            .help("Usages an image which has Jupyter preinstalled")
+            .long("jupyter")
+        )
+        .arg(Arg::with_name("name")
+            .set(ArgSettings::Global)
+            .help("Gives NAME to the container when it is launched")
+            .long("name")
+            .takes_value(true)
+        )
+        .arg(Arg::with_name("port")
+            .set(ArgSettings::Global)
+            .help("Specifies a port mapping for the container and host")
+            .short("p")
+            .long("port")
+            .takes_value(true)
+        )
+        .arg(Arg::with_name("python3")
+            .set(ArgSettings::Global)
+            .help("Uses an image which supports Python3")
+            .long("python3")
+        )
+        .arg(Arg::with_name("root")
+            .set(ArgSettings::Global)
+            .help("Run the docker container as root")
+            .long("root")
+        )
+        .get_matches();
+
     let config = Config::read().map_err(Error::Configure)?;
     let toolchain_override = toolchain::toolchain_override();
 
@@ -47,89 +163,33 @@ fn main_() -> Result<(), Error> {
             },
         );
 
-    let arguments: Vec<String> = args().skip(1).collect();
-    let mut arguments = arguments.iter();
-
-    // Allow the first argument, if it begins with `+`, to override the tag.
-    let mut subcommand = arguments.next().and_then(|argument| {
-        if argument.starts_with('+') {
-            tag = &argument[1..];
-            arguments.next().map(String::as_str)
-        } else if argument.starts_with('=') {
-            specific_container = Some(&argument[1..]);
-            arguments.next().map(String::as_str)
+    if let Some(tag_or_container) = matches.value_of("tag-or-container") {
+        if tag_or_container.starts_with('+') {
+            tag = &tag_or_container[1..];
+        } else if tag_or_container.starts_with('=') {
+            specific_container = Some(&tag_or_container[1..]);
         } else {
-            Some(argument.as_str())
-        }
-    });
-
-    let subcommand = subcommand
-        .take()
-        .context("tensorman must be given a subcommand to execute")
-        .map_err(Error::ArgumentUsage)?;
-
-    let mut subcommand_args = Vec::new();
-
-    let mut as_root = false;
-    let mut force = false;
-
-    let mut flagged_variants = TagVariants::empty();
-
-    let mut name = None;
-    let mut port = None;
-
-    let mut docker_cmd = "docker";
-
-    while let Some(argument) = arguments.next() {
-        match argument.as_str() {
-            "-h" | "--help" => help(),
-            "--" => break,
-            "-f" | "--force" => force = true,
-            "--gpu" => flagged_variants |= TagVariants::GPU,
-            "--https" => {},
-            "--docker-cmd" => {
-                docker_cmd =
-                    arguments
-                        .next()
-                        .context("the --docker-cmd flag requires an argument")
-                        .map_err(Error::ArgumentUsage)?
-                        .as_str()
-            }
-            "--jupyter" => flagged_variants |= TagVariants::JUPYTER,
-            "--name" => {
-                name = Some(
-                    arguments
-                        .next()
-                        .context("the --name flag requires a name as an argument")
-                        .map_err(Error::ArgumentUsage)?
-                        .as_str(),
-                );
-            }
-            "-p" | "--port" => {
-                port = Some(
-                    arguments
-                        .next()
-                        .context("the --port flag requires an argument")
-                        .map_err(Error::ArgumentUsage)?
-                        .as_str(),
-                );
-            }
-            "--python3" => flagged_variants |= TagVariants::PY3,
-            "--root" => as_root = true,
-            argument => {
-                if argument.starts_with('-') {
-                    eprintln!("unknown argument to tensorman: {}", argument);
-                    help();
-                }
-
-                subcommand_args.push(argument)
-            }
+            unreachable!();
         }
     }
 
-    subcommand_args.extend(arguments.map(|x| x.as_str()));
-    let mut subcommand_args = subcommand_args.into_iter();
+    let force = matches.is_present("force");
+    let as_root = matches.is_present("root");
 
+    let docker_cmd = matches.value_of("docker-cmd").unwrap_or("docker");
+    let name = matches.value_of("name");
+    let port = matches.value_of("port");
+
+    let mut flagged_variants = TagVariants::empty();
+    if matches.is_present("gpu") {
+        flagged_variants |= TagVariants::GPU;
+    }
+    if matches.is_present("jupyter") {
+        flagged_variants |= TagVariants::JUPYTER;
+    }
+    if matches.is_present("python3") {
+        flagged_variants |= TagVariants::PY3;
+    }
     if !flagged_variants.is_empty() {
         variants = flagged_variants;
     }
@@ -144,165 +204,68 @@ fn main_() -> Result<(), Error> {
 
     let mut runtime = Runtime::new(docker_cmd).map_err(Error::Docker)?;
 
-    match subcommand {
-        "default" => {
-            let tag = subcommand_args
-                .next()
-                .context("a tag must be provided for the default subcommand")
-                .map_err(Error::ArgumentUsage)?;
+    if let Some(sub_m) = matches.subcommand_matches("default") {
+        let tag = sub_m.value_of("tag").unwrap();
 
-            let source = if tag.starts_with('=') {
-                ImageSourceBuf::Container(tag[1..].into())
-            } else {
-                ImageSourceBuf::Tensorflow(tag.into())
-            };
+        let source = if tag.starts_with('=') {
+            ImageSourceBuf::Container(tag[1..].into())
+        } else {
+            ImageSourceBuf::Tensorflow(tag.into())
+        };
 
-            let variants = subcommand_args.collect::<TagVariants>();
+        let variants = if let Some(variants) = sub_m.values_of("variant") {
+            variants.collect()
+        } else {
+            TagVariants::empty()
+        };
 
-            let new_config =
-                Config { docker_flags: None, image: Some(ImageBuf { variants, source }) };
+        let new_config = Config { docker_flags: None, image: Some(ImageBuf { variants, source }) };
 
-            new_config.write().map_err(Error::Configure)?;
+        new_config.write().map_err(Error::Configure)?;
+    } else if let Some(_sub_m) = matches.subcommand_matches("list") {
+        runtime.list().map_err(Error::Docker)?;
+    } else if let Some(sub_m) = matches.subcommand_matches("pull") {
+        if let Some(tag) = sub_m.value_of("tag") {
+            image.source = ImageSource::Tensorflow(tag);
+            image.variants = flagged_variants;
         }
-        "list" => {
-            runtime.list().map_err(Error::Docker)?;
-        }
-        "pull" => {
-            if let Some(tag) = subcommand_args.next() {
-                image.source = ImageSource::Tensorflow(tag);
-                image.variants = flagged_variants;
-            }
 
-            image.pull(docker_cmd).context("failed to pull image").map_err(Error::Docker)?;
-        }
-        "remove" => {
-            if subcommand_args.len() == 0 {
-                return Err(Error::ArgumentUsage(anyhow!(
-                    "an image must be provided for the remove subcommand"
-                )));
-            }
-
-            for image in subcommand_args {
-                runtime
-                    .remove(image, force)
-                    .with_context(|| format!("failed to remove container '{}'", image))
-                    .map_err(Error::Docker)?;
-            }
-        }
-        "run" => {
-            let cmd = subcommand_args
-                .next()
-                .context("run subcommand requires a command argument")
-                .map_err(Error::ArgumentUsage)?;
-
-            let args: Vec<&str> = subcommand_args.collect();
-            let args: Option<&[&str]> = if args.is_empty() { None } else { Some(&args) };
-            let dflags = config.docker_flags.as_ref().map(Vec::as_slice);
-
+        image.pull(docker_cmd).context("failed to pull image").map_err(Error::Docker)?;
+    } else if let Some(sub_m) = matches.subcommand_matches("remove") {
+        for image in sub_m.values_of("image").unwrap() {
             runtime
-                .run(&image, cmd, name, port, as_root, args, dflags)
-                .context("failed to run container")
+                .remove(image, force)
+                .with_context(|| format!("failed to remove container '{}'", image))
                 .map_err(Error::Docker)?;
         }
-        "save" => {
-            let container = subcommand_args
-                .next()
-                .context("save subcommand requires a container name as a source")
-                .map_err(Error::ArgumentUsage)?;
+    } else if let Some(sub_m) = matches.subcommand_matches("run") {
+        let cmd = sub_m.value_of("cmd").unwrap();
+        let args = sub_m.values_of("arg").map(|x| x.collect::<Vec<_>>());
+        let dflags = config.docker_flags.as_ref().map(Vec::as_slice);
 
-            let image = subcommand_args
-                .next()
-                .context("save subcommand requires an image name as the destination")
-                .map_err(Error::ArgumentUsage)?;
+        runtime
+            .run(&image, cmd, name, port, as_root, args.as_ref().map(|x| x.as_ref()), dflags)
+            .context("failed to run container")
+            .map_err(Error::Docker)?;
+    } else if let Some(sub_m) = matches.subcommand_matches("save") {
+        let container = sub_m.value_of("container").unwrap();
+        let image = sub_m.value_of("image").unwrap();
 
-            runtime
-                .save(container, image)
-                .with_context(|| format!("failed to save container '{}' as '{}'", container, image))
-                .map_err(Error::Docker)?;
-        }
-        "show" => {
-            if subcommand_args.len() == 0 {
-                println!("{}", image);
-            } else {
-                unimplemented!()
-            }
-        }
-        _ => help(),
+        runtime
+            .save(container, image)
+            .with_context(|| format!("failed to save container '{}' as '{}'", container, image))
+            .map_err(Error::Docker)?;
+    } else if let Some(_sub_m) = matches.subcommand_matches("show") {
+        println!("{}", image);
+    } else {
+        unreachable!();
     }
 
     Ok(())
 }
 
-const HELP: &str = "tensorman
-    Tensorflow Docker image manager
-
-USAGE:
-    tensorman [+TAG | =CONTAINER] SUBCOMMAND [FLAGS...]
-
-SUBCOMMANDS:
-    default TAG [VARIANTS...]
-        Defines the default tensorflow image to use when not specified
-
-    list
-        List tensorflow images installed on the system
-
-    pull [TAG]
-        Fetches and updates tensorflow images
-
-    remove ID
-        Removes an image either by its sha sum, or tag
-
-    run COMMAND [-- ARGS...]
-        Mounts an image and executes the given command.
-
-        Use a shell as the COMMAND interactive sessions.
-
-    save CONTAINER NAME
-        Saves an active container with the name of CONTAINER to a new image
-        which shall be named NAME.
-
-    show
-        Show the active image that will be run
-
-FLAGS:
-    -f, --force
-        Apply the subcommand by force (ie: force removal)
-
-    --gpu
-        Uses an image which supports GPU compute
-    
-    --docker-cmd COMMAND
-        Call COMMAND when invoking docker
-
-    --jupyter
-        Usages an image which has Jupyter preinstalled
-
-    --name NAME
-        Gives NAME to the container when it is launched
-
-    -p, --port
-        Specifies a port mapping for the container and host
-
-    --python3
-        Uses an image which supports Python3
-
-    --root
-        Run the docker container as root
-
-    -h, --help
-        Display this information";
-
-fn help() -> ! {
-    println!("{}", HELP);
-    exit(0);
-}
-
 fn main() {
     if let Err(why) = main_() {
-        if let Error::ArgumentUsage(_) = why {
-            println!("{}\n\n", HELP)
-        }
-
         eprintln!("tensorman: {}", why);
         let mut source = why.source();
         while let Some(why) = source {
